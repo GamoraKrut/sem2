6014c7104a10d5f854bae229faddae0ad6453c88
GamoraKrut <arturgamurar1488@gmail.com>
2022-12-19 20:48:07 +0300

    zadacha 1

---
+++ Seminarmaster/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Console.Clear();
+// int num = new Random().Next(10, 100);
+// Console.WriteLine(num);
+// if (num / 10 > num % 10)
+// {
+//     Console.WriteLine(num / 10);
+// }
+// else
+// {
+//     Console.WriteLine(num % 0);
+// }
