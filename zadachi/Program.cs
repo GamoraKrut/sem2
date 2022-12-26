9857fd129a6a2cec53cf07274aefbd8cb4f79724
GamoraKrut <arturgamurar1488@gmail.com>
2022-12-26 19:43:27 +0300

    DZ k sem 2

--- zadachi/Program.cs
+++ zadachi/Program.cs
@@ -74,86 +74,140 @@
 // }
 
 
-DZ sem 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел:
+// DZ sem 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел:
+
+// Console.Clear();
+
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+// int C = Convert.ToInt32(Console.ReadLine());
+
+// if (A > B && A > C)
+// {
+//     Console.WriteLine(A);
+// }
+// else if (B > A && B > C)
+// {
+//     Console.WriteLine(B);
+// }
+// else if (C > A && C > B)
+// {
+//     Console.WriteLine(C);
+// }
+// else if (A == B)
+// {
+//     Console.WriteLine($"{A}");
+// }
+// else if (A == C)
+// {
+//     Console.WriteLine($"{A}");
+// }
+// else if (C == B)
+// {
+//     Console.WriteLine($"{C}");
+// }
+
+// DZ sem 1: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка):
+
+// Console.Clear();
+
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// if (A == 0)
+// {
+//     Console.WriteLine("It's zero");
+// }
+// else if (A % 2 == 0)
+// {
+//     Console.WriteLine("Its's even");
+// }
+// else
+// {
+//     Console.WriteLine("It's not even");
+// }
+
+// DZ sem 1: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N:
+
+// Console.Clear();
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// int I = 0;
+
+// if (N == 0)
+// {
+//     Console.WriteLine("It's zero, dumbass");
+// }
+// else if (N == 1 || N == -1)
+// {
+//     Console.WriteLine("There are no any even numbers before 1, morron");
+// }
+// else if (N < I)
+// {
+//     while (I > (N + 1))
+//     {
+//         I -= 2;
+//         Console.WriteLine(I);
+//     }
+// }
+// else
+// {
+//     while (I < (N - 1))
+//     {
+//         I += 2;
+//         Console.WriteLine(I);
+//     }
+// }
+
+//DZ sem 2:  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа:
+
+// Console.Clear();
+// Console.Write("Ввести трехзначное число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// if (A > 999 || A < 100)
+// {
+//     Console.WriteLine("Трехзначное не больше, не меньше");
+// }
+// else
+// {
+//     int B = A % 100;
+//     B = B / 10;
+//     Console.WriteLine(B);
+// }
+
+// DZ sem 2:Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет:
+// Console.Clear();
+// Console.Write("Input number: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// if (A < 100)
+// {
+//     Console.WriteLine("Input number bigger than 99");
+// }
+// else
+// {
+//     while (A >= 1000)
+//     {
+//         A = A / 10;
+//     }
+//     int B = A % 10;
+//     Console.WriteLine($"Third number is: {B}");
+// }
+
+// DZ sem 2:Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным:
 
 Console.Clear();
-
-int A = Convert.ToInt32(Console.ReadLine());
-int B = Convert.ToInt32(Console.ReadLine());
-int C = Convert.ToInt32(Console.ReadLine());
-
-if (A > B && A > C)
-{
-    Console.WriteLine(A);
-}
-else if (B > A && B > C)
-{
-    Console.WriteLine(B);
-}
-else if (C > A && C > B)
-{
-    Console.WriteLine(C);
-}
-else if (A == B)
-{
-    Console.WriteLine($"{A}");
-}
-else if (A == C)
-{
-    Console.WriteLine($"{A}");
-}
-else if (C == B)
-{
-    Console.WriteLine($"{C}");
-}
-
-DZ sem 1: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка):
-
-Console.Clear();
-
+Console.Write("Input number: ");
 int A = Convert.ToInt32(Console.ReadLine());
 
-if (A == 0)
+if (A > 7 || A < 1)
 {
-    Console.WriteLine("It's zero");
+    Console.WriteLine("Incorrect day");
 }
-else if (A % 2 == 0)
+else if (A > 5)
 {
-    Console.WriteLine("Its's even");
+    Console.WriteLine("Yes it's holiday");
 }
 else
 {
-    Console.WriteLine("It's not even");
-}
-
-DZ sem 1: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N:
-
-Console.Clear();
-
-int N = Convert.ToInt32(Console.ReadLine());
-int I = 0;
-
-if (N == 0)
-{
-    Console.WriteLine("It's zero, dumbass");
-}
-else if (N == 1 || N == -1)
-{
-    Console.WriteLine("There are no any even numbers before 1, morron");
-}
-else if (N < I)
-{
-    while (I > (N + 1))
-    {
-        I -= 2;
-        Console.WriteLine(I);
-    }
-}
-else
-{
-    while (I < (N - 1))
-    {
-        I += 2;
-        Console.WriteLine(I);
-    }
+    Console.WriteLine("No it's weekday");
 }
