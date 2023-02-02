5224ba120c41b0569da3f1ab47b9268273bdacc1
GamoraKrut <arturgamurar1488@gmail.com>
2023-02-02 03:17:10 +0300

    dz sem 6

--- zadachi/Program.cs
+++ zadachi/Program.cs
@@ -1023,9 +1023,9 @@
 //     UserArray[i] = Convert.ToInt32(Console.ReadLine());
 // }
 
-// int CountPos(int[] Array)
+// int CountPositiveNumbers(int[] Array)
 // {
-//     int j = 1;
+//     int j = 0;
 //     foreach (int i in Array)
 //     {
 //         if (i > 0)
@@ -1036,7 +1036,7 @@
 //     return j;
 // }
 
-// void Show(int[] Arr)
+// void ShowArray(int[] Arr)
 // {
 //     for (int i = 0; i < Arr.Length; i++)
 //     {
@@ -1045,22 +1045,52 @@
 //     Console.WriteLine();
 // }
 
-// Show(UserArray);
-// Console.WriteLine($"Введено чисел > 0: {CountPos(UserArray)}");
+// ShowArray(UserArray);
+// Console.WriteLine($"Введено чисел > 0: {CountPositiveNumbers(UserArray)}");
 
 // // Zad 2:  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
 // //         y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-Console.Clear();
+// Console.Clear();
 
-Console.Write("Введите значение b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите значение b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Введите значение k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите значение k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Введите значение b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите значение b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Введите значение k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите значение k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+
+// if (b1 == b2 && k1 == k2)
+// {
+//     Console.WriteLine("Прямые лежат друг на друге");
+// }
+// else if (b1 == b2 || k1 == k2)
+// {
+//     Console.WriteLine("Прямые параллельны");
+// }
+// else
+// {
+//     double[] CrossPoint(double ArrB1, double ArrK1, double ArrB2, double ArrK2)
+//     {
+//         double[] result = new double[2];
+//         result[0] = (ArrB2 - ArrB1) / (ArrK1 - ArrK2);
+//         result[1] = ArrK1 * result[0] + ArrB1;
+//         return result;
+//     }
+
+//     void ShowArray(double[] Arr)
+//     {
+//         for (int i = 0; i < Arr.Length; i++)
+//         {
+//             Console.Write(Arr[i] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+
+//     ShowArray(CrossPoint(b1, k1, b2, k2));
+// }
