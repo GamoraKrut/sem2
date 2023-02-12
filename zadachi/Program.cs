138b8f039818f4ceb8c9d2f92eb10f8bd1d994d2
GamoraKrut <arturgamurar1488@gmail.com>
2023-02-13 01:06:56 +0300

    dz sem 7 dz 3 oshibka

--- zadachi/Program.cs
+++ zadachi/Program.cs
@@ -1094,3 +1094,194 @@
 
 //     ShowArray(CrossPoint(b1, k1, b2, k2));
 // }
+
+// Sem 7:
+
+//Zad 1: Задать двумерный массив размером m x n, заполненный случайными целыми числами.
+
+// Console.Clear();
+
+// Console.Write("Введите колчиство строк: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите колчиство столбцов: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+
+// int[,] GetArray(int m, int n, int MinValue, int MaxValue)
+// {
+//     int[,] result = new int [m, n];
+//     for(int i = 0; i < m; i++)
+//     {
+//         for(int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(MinValue, MaxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void
+
+
+
+
+// DZ sem 7:
+
+// zad 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// Console.Clear();
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// double[,] Array = RandomArray(rows, columns, 0, 100);
+// PrintArray(Array);
+
+// double[,] RandomArray(int m, int n, int start, int end)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble() * (start - end);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(double[,] Array)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             Console.Write($"{Array[i, j]:f3} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// zad 2: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Console.Clear();
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение элемента: ");
+// int element = Convert.ToInt32(Console.ReadLine());
+
+// int[,] Array = RandomArray(rows, columns, 0, 100);
+// PrintArray(Array);
+
+// if (ElementSearch(Array, element))
+// {
+//     Console.WriteLine("Элемент найден");
+// }
+// else
+// {
+//     Console.WriteLine("Элемент не найден");
+// }
+
+// int[,] RandomArray(int m, int n, int start, int end)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(start, end + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] Array)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             Console.Write($"{Array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// bool ElementSearch(int[,] Array, int el)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             if (Array[i, j] == el)
+//             {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// zad 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = RandomArray(rows, columns, 0, 100);
+PrintArray(Array);
+
+double[] MiddleSum = ResultArray(Array);
+Console.WriteLine($"Среднее Арифметическое столбцов = {String.Join("; ", MiddleSum)}");
+
+int[,] RandomArray(int m, int n, int start, int end)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ResultArray(int[,] Array)
+{
+    double[] result = new double[Array.GetLength(1)];
+    for (int i = 0; i < Array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < Array.GetLength(0); j++)
+        {
+            sum += Array[j, i];
+        }
+        result[i] = Math.Round(sum / Array.GetLength(0), 2);
+    }
+    return result;
+}
